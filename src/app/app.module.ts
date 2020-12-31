52065dc24c9f9f850690e9ae2eb79816646084ca
pablohs1986 <pabloherrero1986@gmail.com>
2020-12-31 17:48:21 +0100

    Component edited and imports done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CorreoComponent } from './Components/correo/correo.component';
@@ -16,7 +16,8 @@ import { NuevoCorreoComponent } from './Components/nuevo-correo/nuevo-correo.com
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
